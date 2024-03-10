f967293b85c9cc6dd76b079a8c29872051e2a4db
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-03-10 12:29:03 -0700

    github source link

--- src/components/Sources.svelte
+++ src/components/Sources.svelte
@@ -1,21 +1,22 @@
-<div style="text-align: left; margin-left: 8%; margin-right: 8%;">
-    <h1 style="font-family: Futura; line-height: 3;">Sources</h1>
-
-    <div style="font-family: Verdana;">
-        <p><a href="https://en.wikipedia.org/wiki/Anxiety_disorder" style="color: black;">https://en.wikipedia.org/wiki/Anxiety_disorder</a></p>
-        <p><a href="https://psychcentral.com/anxiety/the-origins-of-anxiety#medication" style="color: black;">https://psychcentral.com/anxiety/the-origins-of-anxiety#medication</a></p>
-        <p><a href="https://www.kaggle.com/datasets/shariful07/student-mental-health" style="color: black;">https://www.kaggle.com/datasets/shariful07/student-mental-health</a></p>
-        <p><a href="https://www.sciencedirect.com/science/article/pii/S2666915323001968#:~:text=In%20the%20more%20recent%20Spring,_phobia" style="color: black;">https://www.sciencedirect.com/science/article/pii/S2666915323001968#:~:text=In%20the%20more%20recent%20Spring,_phobia</a></p>
-        <p><a href="https://www.thementalhealthcoalition.org/college-mental-health-toolkit/?gad_source=1&gclid=Cj0KCQiArrCvBhCNARIsAOkAGcXeBr7vCMWAt-eB4TulqfxL7LWFjdjpbaagxIY1jnOTKCNvCr3Sbj4aAp5lEALw_wcB" style="color: black;">https://www.thementalhealthcoalition.org/college-mental-health-toolkit/?gad_source=1&gclid=Cj0KCQiArrCvBhCNARIsAOkAGcXeBr7vCMWAt-eB4TulqfxL7LWFjdjpbaagxIY1jnOTKCNvCr3Sbj4aAp5lEALw_wcB</a></p>
-        <p><a href="https://ourworldindata.org/grapher/anxiety-disorders-prevalence-by-age?tab=table&time=1990..2019" style="color: black;">https://ourworldindata.org/grapher/anxiety-disorders-prevalence-by-age?tab=table&time=1990..2019</a></p>
-        <p><a href="https://www.goodrx.com/conditions/generalized-anxiety-disorder/how-is-age-related-to-anxiety#young-to-middle-age-adults" style="color: black;">https://www.goodrx.com/conditions/generalized-anxiety-disorder/how-is-age-related-to-anxiety#young-to-middle-age-adults</a></p>
-        <p><a href="https://ourworldindata.org/grapher/anxiety-disorders-prevalence-males-vs-females" style="color: black;">https://ourworldindata.org/grapher/anxiety-disorders-prevalence-males-vs-females</a></p>
-        <p><a href="https://rightasrain.uwmedicine.org/mind/mental-health/men-mental-health" style="color: black;">https://rightasrain.uwmedicine.org/mind/mental-health/men-mental-health</a></p>
-        <p><a href="https://www.priorygroup.com/blog/40-of-men-wont-talk-to-anyone-about-their-mental-health" style="color: black;">https://www.priorygroup.com/blog/40-of-men-wont-talk-to-anyone-about-their-mental-health</a></p>
-        <p><a href="https://www.mayoclinic.org/diseases-conditions/anxiety/diagnosis-treatment/drc-20350967" style="color: black;">https://www.mayoclinic.org/diseases-conditions/anxiety/diagnosis-treatment/drc-20350967</a></p>
-        <p><a href="https://www.clinical-partners.co.uk/for-adults/anxiety-disorders/anxiety-test?restart=40304d68dd65b6a92a4ddfe8e1748969" style="color: black;">https://www.clinical-partners.co.uk/for-adults/anxiety-disorders/anxiety-test?restart=40304d68dd65b6a92a4ddfe8e1748969</a></p>
-
+<div style="text-align: left; margin-left: 8%; margin-right: 8%; display: flex; flex-wrap: wrap;">
+    <div style="width: 50%; font-family: Verdana;">
+        <h1 style="font-family: Futura; line-height: 3;">Sources</h1>
+        <p><a href=" " style="color: black;">Wikipedia: Anxiety Disorder</a></p>
+        <p><a href="https://psychcentral.com/anxiety/the-origins-of-anxiety#medication" style="color: black;">Psych Central: The Origins of Anxiety</a></p>
+        <p><a href="https://www.kaggle.com/datasets/shariful07/student-mental-health" style="color: black;">Kaggle: Student Mental Health Dataset</a></p>
+        <p><a href="https://www.sciencedirect.com/science/article/pii/S2666915323001968#:~:text=In%20the%20more%20recent%20Spring,_phobia" style="color: black;">ScienceDirect: Article on Anxiety Disorders</a></p>
+        <p><a href="https://www.thementalhealthcoalition.org/college-mental-health-toolkit/?gad_source=1&gclid=Cj0KCQiArrCvBhCNARIsAOkAGcXeBr7vCMWAt-eB4TulqfxL7LWFjdjpbaagxIY1jnOTKCNvCr3Sbj4aAp5lEALw_wcB" style="color: black;">The Mental Health Coalition: College Mental Health Toolkit</a></p>
+        <p><a href="https://ourworldindata.org/grapher/anxiety-disorders-prevalence-by-age?tab=table&time=1990..2019" style="color: black;">Our World in Data: Anxiety Disorders Prevalence by Age</a></p>
+        <h3 style="font-family: Futura; line-height: 5; font-weight: 500;">Source Code: <span style="font-size: inherit;"><a href="https://github.com/ylesia-wu/dsc106-final-project" style="color: black; font-weight: 100;">Github</a></span></h3>
     </div>
     
-    
+    <div style="width: 50%; font-family: Verdana; margin-top: 7.5em;">
+        <p><a href="https://www.goodrx.com/conditions/generalized-anxiety-disorder/how-is-age-related-to-anxiety#young-to-middle-age-adults" style="color: black;">GoodRx: How Is Age Related to Anxiety</a></p>
+        <p><a href="https://ourworldindata.org/grapher/anxiety-disorders-prevalence-males-vs-females" style="color: black;">Our World in Data: Anxiety Disorders Prevalence - Males vs Females</a></p>
+        <p><a href="https://rightasrain.uwmedicine.org/mind/mental-health/men-mental-health" style="color: black;">Right as Rain by UW Medicine: Men and Mental Health</a></p>
+        <p><a href="https://www.priorygroup.com/blog/40-of-men-wont-talk-to-anyone-about-their-mental-health" style="color: black;">Priory Group: 40% of Men Won't Talk to Anyone About Their Mental Health</a></p>
+        <p><a href="https://www.mayoclinic.org/diseases-conditions/anxiety/diagnosis-treatment/drc-20350967" style="color: black;">Mayo Clinic: Anxiety Diagnosis & Treatment</a></p>
+        <p><a href="https://www.clinical-partners.co.uk/for-adults/anxiety-disorders/anxiety-test?restart=40304d68dd65b6a92a4ddfe8e1748969" style="color: black;">Clinical Partners: Anxiety Test for Adults</a></p>
+        
+    </div>
 </div>
