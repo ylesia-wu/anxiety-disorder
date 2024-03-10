cd1eaedef17d5ddfa6ecaf79e5e17ad0a7ac476d
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-03-09 22:48:37 -0800

    Update Symptoms.svelte

--- src/components/Symptoms.svelte
+++ src/components/Symptoms.svelte
@@ -1,9 +1,5 @@
 <main style="margin-left: 150px; margin-right: 150px;">
         <h1 style="font-family: Futura; font-size: 34px; margin-bottom: 20px; line-height: 1.6; text-align: left;">Check yourself if you have symptoms below … </h1>
-        <div style="font-family: Verdana; font-size: 18px; text-align: justify; line-height: 1.6;">
-            <p>Data suggests that a higher share of the female population has an anxiety disorder compared to males. However, data also shows that the share of the male population with anxiety is not significantly less than that of females. In the United States during the past decade, the percentage of males with anxiety has been around two-thirds of that of females, representing tens of millions of individuals.</p>
-            <p>Some men may avoid discussing the anxiety they experience if they don't perceive it as serious enough for intervention. Approximately 40% of men have never spoken to anyone about their mental health. A likely reason for this phenomenon is the gender stereotype that still prevails in our society.</p>
-        </div>
 
         <style>
     .columns {
@@ -15,8 +11,8 @@
     }
 </style>
 
-<div class="columns">
-    <div class="column">
+<div class="columns" style="font-family: Verdana; font-size: 18px; text-align: justify; line-height: 1.6;">
+    <div class="column" style="width: 600px; margin-left: -10px">
         
         <ul>
             <li>Feeling nervous, restless or tense</li>
@@ -24,11 +20,12 @@
             <li>Having an increased heart rate</li>
             <li>Breathing rapidly (hyperventilation)</li>
             <li>Sweating</li>
+            <li>Trembling</li>
         </ul>
     </div>
-    <div class="column">
+    <div class="column" style="width: 600px; margin-left: 10px">
         <ul>
-            <li>Trembling</li>
+            
             <li>Feeling weak or tired</li>
             <li>Trouble concentrating or thinking about anything other than the present worry</li>
             <li>Having trouble sleeping</li>
