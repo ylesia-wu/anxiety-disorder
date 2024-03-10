5c4774b1289022ba4326e1fcd58b048782f5bc20
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-03-10 12:55:52 -0700

    Update Sources.svelte

--- src/components/Sources.svelte
+++ src/components/Sources.svelte
@@ -1,5 +1,5 @@
 <div style="text-align: left; margin-left: 8%; margin-right: 8%; display: flex; flex-wrap: wrap;">
-    <div style="width: 50%; font-family: Verdana;">
+    <div style="width: 45%; font-family: Verdana;">
         <h1 style="font-family: Futura; line-height: 3;">Sources</h1>
         <p><a href=" " style="color: black;">Wikipedia: Anxiety Disorder</a></p>
         <p><a href="https://psychcentral.com/anxiety/the-origins-of-anxiety#medication" style="color: black;">Psych Central: The Origins of Anxiety</a></p>
@@ -10,7 +10,7 @@
         <h3 style="font-family: Futura; line-height: 5; font-weight: 500;">Source Code: <span style="font-size: inherit;"><a href="https://github.com/ylesia-wu/dsc106-final-project" style="color: black; font-weight: 100;">Github</a></span></h3>
     </div>
     
-    <div style="width: 50%; font-family: Verdana; margin-top: 7.5em;">
+    <div style="width: 55%; font-family: Verdana; margin-top: 7.9em;">
         <p><a href="https://www.goodrx.com/conditions/generalized-anxiety-disorder/how-is-age-related-to-anxiety#young-to-middle-age-adults" style="color: black;">GoodRx: How Is Age Related to Anxiety</a></p>
         <p><a href="https://ourworldindata.org/grapher/anxiety-disorders-prevalence-males-vs-females" style="color: black;">Our World in Data: Anxiety Disorders Prevalence - Males vs Females</a></p>
         <p><a href="https://rightasrain.uwmedicine.org/mind/mental-health/men-mental-health" style="color: black;">Right as Rain by UW Medicine: Men and Mental Health</a></p>
